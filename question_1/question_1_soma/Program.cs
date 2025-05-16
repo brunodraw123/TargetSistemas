9f15fe8d31ac81911d47e6a6e12381cb49d3f7f6
Bruno Nascimento de Oliveira <86574824+brunodraw123@users.noreply.github.com>
2025-05-15 22:46:43 -0300

    first question
    first question

---
+++ question_1/question_1_soma/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+
+1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
+Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+Imprimir(SOMA);
+Ao final do processamento, qual será o valor da variável SOMA?
+
+
+int INDICE = 13, SOMA = 0, K = 0;
+Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+Imprimir(SOMA);
+*/
+var indice = 13;
+var soma = 0;
+var k = 0;
+
+while (k < indice)
+{
+    k = k + 1;
+    soma = soma + k;
+}
+
+Console.WriteLine($"SOMA = {soma}"); //91
+Console.ReadKey();
