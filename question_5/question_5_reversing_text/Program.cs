6566509cc5ce1ce0f5ac99da9cfba8364b67bd36
Bruno Nascimento de Oliveira <86574824+brunodraw123@users.noreply.github.com>
2025-05-15 22:58:10 -0300

    fiveth question
    fiveth question

---
+++ question_5/question_5_reversing_text/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Inverter texto digitado");
+string input = Console.ReadLine();
+var inputToReverse = "";
+
+
+char[] arrayOfInput = input.ToCharArray();
+
+
+for (int i = arrayOfInput.Length - 1; i >= 0; i--)
+{
+    //Console.Write(arrayOfInput[i]);
+    inputToReverse = inputToReverse + arrayOfInput[i];    
+}
+
+Console.WriteLine("Texto reverso: " + inputToReverse);
+
+Console.ReadLine();
+Console.Clear();
